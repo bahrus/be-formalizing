fe26a9568f6ec95ae4db4367e0994471daabc989
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-01 10:08:53 -0500

    Update README.md

---
+++ be-formalizing.js
@@ -0,0 +1,31 @@
+// @ts-check
+import { resolved, rejected, propInfo} from 'be-enhanced/cc.js';
+import { BE } from 'be-enhanced/BE.js';
+import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
+
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AllProps, AP, BAP, ITyper} from './ts-refs/be-formalizing/types.d.ts' */;
+
+/**
+ * @implements {Actions}
+ * 
+ */
+class BeTyped extends BE {
+        /**
+     * @type {BEConfig<BAP, Actions & IEnhancement>}
+     */
+    static config = {
+    };
+
+    de = de;
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async addFormalizeBtn(self){
+        return /** @type {PAP} */ ({
+        });
+    }
+}
