f1ebccdd7a4acac2e8e792d9d0c3f82eff8478e7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-01 10:31:45 -0500

    Update README.md

--- be-formalizing.js
+++ be-formalizing.js
@@ -40,8 +40,25 @@ class BeFormalizing extends BE {
      * @returns 
      */
     async addFormalizeBtn(self){
+        let byob = true;
+        const {triggerInsertPosition, enhancedElement} = self;
+        const {findAdjacentElement} = await import('trans-render/lib/findAdjacentElement.js');
+        let trigger = /** @type {HTMLButtonElement | null} */ (findAdjacentElement(triggerInsertPosition, enhancedElement, 'button.be-clonable-trigger'));
+        if(trigger === null){
+            byob = false;
+            trigger = document.createElement('button');
+            trigger.type = 'button';
+            trigger.classList.add('be-formalizing-trigger');
+            trigger.ariaLabel = 'Configure form.';
+            trigger.title = 'Configure form.';
+            enhancedElement.insertAdjacentElement(triggerInsertPosition, trigger);
+        }
         return /** @type {PAP} */ ({
+            trigger: new WeakRef(trigger),
+            resolved: true,
+            byob
         });
+
     }
 }
 
